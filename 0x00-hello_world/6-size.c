6f2adb9d7795e2c5bc9af82053758d56e3e15971
vuyosibango <vsibango@gmail>
2023-02-18 09:51:00 -0800

    updated size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,15 @@
 */
 int main(void)
 {
-prinf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long long int));
-return (0);
+ prinf("Size of a char: %d byte(s)\n", sizeof(char));
+ 
+ printf("Size of an int: %d byte(s)\n", sizeof(int));
+ 
+ printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+
+ printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+
+ printf("size of a float: %d byte(S)\n", sizeof(float));
+	 
+	 (return (0);
 }
